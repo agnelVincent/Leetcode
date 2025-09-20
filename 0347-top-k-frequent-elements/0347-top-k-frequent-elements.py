3a1bdfca25d0270c23c909d0b2c67230d3efb5b2
Agnel Vincent <agnelvincent779@gmail.com>
2025-09-20 15:49:43 +0530

    Time: 1219 ms (5.15%), Space: 21.1 MB (81.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        for i in nums:
-            if i not in freq:
-                freq[i] = 0
-            freq[i] += 1
 
-        print(freq)
-        x = dict(sorted(freq.items() , key = lambda i : i[1] , reverse = True))
+        x = dict(sorted({i:nums.count(i) for i in set(nums)}.items() , key = lambda i : i[1] , reverse = True)[:k])
         print(x)
-        return [i for i in x.keys()][:k]
+        return [i for i in x]
+
+        
