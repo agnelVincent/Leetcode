bf9c42b96f6f5f36bfc03e31da07cabbd13870bd
Agnel Vincent <agnelvincent779@gmail.com>
2025-09-24 15:38:53 +0530

    Delete 0342-power-of-four directory

--- 0342-power-of-four/0342-power-of-four.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPowerOfFour(self, n: int , x = 0) -> bool:
-        if n == 4**x:
-            return True
-        if x**4 > n:
-            return False
-        return self.isPowerOfFour(n , x+1)
-        
