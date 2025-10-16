70517e2d5ecc9a6c589c317ff58c7c8cd58f3a0c
Agnel Vincent <agnelvincent779@gmail.com>
2025-10-16 15:01:41 +0530

    Time: 2 ms (43.25%), Space: 17.6 MB (97.2%) - LeetHub

---
+++ 3712-sum-of-elements-with-frequency-divisible-by-k/3712-sum-of-elements-with-frequency-divisible-by-k.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumDivisibleByK(self, nums: List[int], k: int) -> int:
+        x = []
+        for i in dict.fromkeys(nums):
+            if nums.count(i) % k == 0:
+                x.append(i)
+        return sum([i for i in nums if i in x])
