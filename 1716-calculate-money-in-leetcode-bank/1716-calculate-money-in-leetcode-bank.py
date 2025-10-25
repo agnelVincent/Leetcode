bc30a4e23d763f1beb7def394fd3d7c59040213c
Agnel Vincent <agnelvincent779@gmail.com>
2025-10-25 11:53:52 +0530

    Time: 23 ms (6.56%), Space: 18 MB (16.22%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        val = 1
+        inc = 0
+        res = 0
+        for i in range(n):
+            if i > 0 and i % 7 == 0:
+                inc += 1
+                val = 1
+            print(val,inc)
+            res += val + inc
+            val += 1
+        return res
