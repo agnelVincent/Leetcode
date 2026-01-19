dfd91ee287a6382497f492f2d4bd196ec56ed915
Agnel Vincent <agnelvincent779@gmail.com>
2026-01-19 15:52:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.46%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.py
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.py
@@ -1,9 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         res = set()
-        i = 0
         steps = 0
-        while i < len(nums) and len(res) != k:
+        while len(nums) and len(res) != k:
             if nums[len(nums)-1] <= k:
                 res.add(nums.pop(-1))
             else:
