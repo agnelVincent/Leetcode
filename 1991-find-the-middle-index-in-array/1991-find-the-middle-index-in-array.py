e28201264776443c64a73be534e0a37136ef286c
Agnel Vincent <agnelvincent779@gmail.com>
2026-02-06 21:53:22 +0530

    Time: 19 ms (4.37%), Space: 19.4 MB (30.23%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -1,7 +1,5 @@
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
         i = 0
         while i < len(nums):
             print(nums[:i],nums[i:])
