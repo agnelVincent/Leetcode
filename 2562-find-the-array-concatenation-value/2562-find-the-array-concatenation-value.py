97481eba5d76814fd3c0d504fc29002b6459c05e
Agnel Vincent <agnelvincent779@gmail.com>
2026-06-28 23:52:38 +0530

    Time: 3 ms (57.73%), Space: 19.4 MB (37.85%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -4,9 +4,7 @@ class Solution:
         i = 0
         while i < len(nums) // 2:
             res += int(str(nums[i]) + str(nums[-1-i]))
-            print(str(nums[i]) + str(nums[-1]))
             i += 1
-            print(f'result is {res}')
         
         if len(nums) % 2 != 0:
             res += nums[i]
