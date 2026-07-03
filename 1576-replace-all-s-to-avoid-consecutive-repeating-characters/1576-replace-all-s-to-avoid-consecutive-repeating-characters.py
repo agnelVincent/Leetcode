9818033096704c27907ea51c47cc9011a8d51058
Agnel Vincent <agnelvincent779@gmail.com>
2026-07-04 00:04:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.44%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def modifyString(self, s: str) -> str:
+        ch = 97
+        res = list(s)
+
+        i = 0
+        while i < len(res):
+            if res[i] == '?':
+                visited = set()
+                if i - 1 >= 0:
+                    visited.add(ord(res[i-1]))
+                if i + 1 < len(res):
+                    visited.add(ord(res[i+1]))
+                for x in range(97,124):
+                    if x not in visited:
+                        res[i] = chr(x)
+                        break
+            i += 1
+        
+        return ''.join(res)
