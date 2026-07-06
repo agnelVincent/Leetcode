09085eadbb241a55bbbf9af4beed3bbcdda0e498
Agnel Vincent <agnelvincent779@gmail.com>
2026-07-06 15:42:52 +0530

    Time: 3 ms (65.67%), Space: 19.4 MB (40.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -14,11 +14,7 @@ class Solution:
         dummy = ListNode(0)
         current = dummy
         left = 0
-
         while True:
-            if not l1 and not l2 and not left:
-                break
-
             val = left
             if l1:
                 val += l1.val
@@ -29,9 +25,13 @@ class Solution:
                 l2 = l2.next
             
             left = val // 10
+            val = val % 10
 
-            current.next = ListNode(val % 10)
+            current.next = ListNode(val)
             current = current.next
+
+            if not l1 and not l2 and not left:
+                break
 
         return dummy.next
             
