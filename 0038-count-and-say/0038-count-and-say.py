cc20fd568253f4d2075fc25083e7ebd256cd2137
Agnel Vincent <agnelvincent779@gmail.com>
2026-07-17 17:42:07 +0530

    Time: 9 ms (25%), Space: 19.2 MB (92.78%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        i = 1
+        res = '1'
+        while i < n:
+            temp_res = ''
+            x = 0
+            while x < len(res):
+                check = res[x]
+                t = x
+                while t < len(res) and res[t] == check:
+                    t += 1
+                temp_res += str(t - x) + check
+                x = t
+            res = temp_res
+            i += 1
+        
+        return res
