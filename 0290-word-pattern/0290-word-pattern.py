73d196822274f940f465710eb9f9cbac84179fe5
Agnel Vincent <agnelvincent779@gmail.com>
2026-08-03 23:39:07 +0530

    Time: 2 ms (3.87%), Space: 19.3 MB (28.79%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        pat = {}
+        pat2 = {}
+        i = 0
+        check = s.split()
+        if len(check) != len(pattern):
+            return False
+        while i < len(check):
+            if check[i] not in pat and pattern[i] not in pat2:
+                pat2[pattern[i]] = check[i]
+                pat[check[i]] = pattern[i]
+            else:
+                if check[i] in pat:
+                    if pattern[i] not in pat2:
+                        return False
+                if pattern[i] in pat2:
+                    if check[i] not in pat:
+                        return False
+                if pat[check[i]] != pattern[i] or pat2[pattern[i]] != check[i]:
+                    return False
+            print(pat, pat2)
+            i += 1
+
+        return True
