11ed059d24af0b26d4b746f78ae8c709405bb435
Agnel Vincent <agnelvincent779@gmail.com>
2026-08-04 23:03:35 +0530

    Time: 1 ms (27.61%), Space: 19.5 MB (8.41%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        i = 0
+        res = []
+        while i < len(words):
+            if len(words[i]) != len(pattern):
+                i += 1
+                continue
+
+            print(i)
+            pat = {}
+            visited = set()
+            x = 0
+            obeys = True
+            while x < len(words[i]):
+                if pattern[x] not in pat:
+                    if words[i][x] in visited:
+                        obeys = False
+                        break
+                    else:
+                        pat[pattern[x]] = words[i][x]
+                        visited.add(words[i][x])
+                else:
+                    if pat[pattern[x]] != words[i][x]:
+                        obeys = False
+                        break
+                x += 1
+            if obeys:
+                res.append(words[i])
+            i += 1
+
+        return res
